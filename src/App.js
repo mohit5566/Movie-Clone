47d93737b1f9382a8e16d3ce99554fb3158c9755
mohit5566 <70192922+mohit5566@users.noreply.github.com>
2021-11-25 16:10:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+import HomePage from './pages/Home.page.jsx';
+import MoviePage from './pages/Movie.page.jsx';
+import DefaultHOC from './HOC/Default.HOC.jsx';
+import MovieHOC from './HOC/Movie.HOC.jsx';
+import PlaysPage from './pages/Plays.pages.jsx';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+//axios default settings
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {};
+axios.defaults.params['api_key'] = process.env.REACT_APP_API_KEY;
+
+function App() {
+  return (
+    <Route>
+      <DefaultHOC path='/' exact component={HomePage} />
+      <MovieHOC path='/movie/:id' exact component={MoviePage} />
+      <DefaultHOC path='/plays' exact component={PlaysPage} />
+    </Route>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import EntertainmentCardSlider from '../components/Entertainment/Entertainmentcard.component.jsx';
+import PosterSlider from '../components/PosterSlider/PosterSlider.component.jsx';
+import axios from 'axios';
+
+const HomePage = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [upcommingMovies, setUpcommingMovies] = useState([]);
+
+  useEffect(() => {
+    const requestPopularMovies = async () => {
+      const getPopularMovies = await axios.get('/movie/popular');
+      console.log('PM', getPopularMovies.data.results);
+    };
+    requestPopularMovies();
+    const requestUpcommingMovies = async () => {
+      const getUpcommingMovies = await axios.get('/movie/upcoming');
+      setUpcommingMovies(getUpcommingMovies.data.results);
+    };
+    requestUpcommingMovies();
+    
+  }, []);
+
+  return (
+    <>
+      <div className='flex flex-col gap-10'>
+        <div className='container mx-auto px-4'>
+          <h1 className='text-2xl font-bold text-gray-800'>
+            The best of Entertainment
+          </h1>
+          <EntertainmentCardSlider />
+        </div>
+
+        <div className='bg-navCol-1000 pr-16'>
+          <div className='container mx-auto px-4'>
+            <div className='flex'>
+              <img
+                src='https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png'
+                alt='Rupay'
+                className='w-full h-full'
+              />
+            </div>
+            <PosterSlider
+              images={popularMovies} 
+              title='Premiers'
+              subtitle='Brand New release every friday'
+              isDark={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='container mx-auto px-4'>
+        <PosterSlider
+          images={upcommingMovies}
+          title='Upcomming Movies'
+          subtitle='Movies Yet to Come'
+          isDark={false}
+        />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
